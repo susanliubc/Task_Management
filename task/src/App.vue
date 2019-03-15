aa2b4c020c2e9f2702628c6da55936d1eabc5662
susanliubc <susanliubc@gmail.com>
2019-03-14 18:05:23 -0700

    add home and about pages

---
+++ task/src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <v-app class="teal lighten-5">
+    <Navbar />
+    <v-content>
+      <router-view></router-view>
+    </v-content>
+  </v-app>
+</template>
+
+<script>
+import Navbar from './components/Navbar';
+
+export default {
+  name: 'App',
+  components: { Navbar },
+  data () {
+    return {
+      //
+    }
+  }
+}
+</script>
---
+++ task/src/components/Navbar.vue
@@ -0,0 +1,15 @@
+<template>
+    <nav>
+        <v-toolbar flat app>
+            <v-toolbar-title class="text-uppercase blue--text">
+                <span>Task</span>
+                <span class="font-weight-light">Management</span>
+            </v-toolbar-title>
+            <v-spacer></v-spacer>
+            <v-btn flat color="blue">
+                <span>Sign Out</span>
+                <v-icon right>exit_to_app</v-icon>
+            </v-btn>
+        </v-toolbar>
+    </nav>
+</template>
